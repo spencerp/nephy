478b845c104d4ea0512e8c4e35f6ff38e9ca1d8e
Richard Huang <richardhuang@Richards-MacBook-Pro.local>
2016-04-23 22:28:06 -0700

    broken api calls

--- index.js
+++ index.js
@@ -5,6 +5,104 @@
 
 // When not cloning the `node-wit` repo, replace the `require` like so:
 const Wit = require('node-wit').Wit;
+const http = require('http');
+
+
+const getNutrientFacts = ((id, context, callback) => {
+
+  http.get("http://api.nal.usda.gov/ndb/reports/?ndbno=18003&type=f&format=json&api_key=hhrhGfhytRdiE3nDCPKuKU1xx1t3u1eGGFcz2igy"
+  //   {
+  //     host: 'api.nal.usda.gov',
+  //     path: '/ndb/reports/?ndbno=' + id + '&type=f&format=json&api_key=hhrhGfhytRdiE3nDCPKuKU1xx1t3u1eGGFcz2igy'
+  // }
+  , function(response) {
+      // Continuously update stream with data
+      var body = '';
+      response.on('data', function(d) {
+          body += d;
+      });
+      response.on('end', function() {
+        // Data reception is done, do whatever with it!
+        var parsed = JSON.parse(body);
+        console.log(parsed);
+        console.log(parsed.report.food.nutrients);
+        var sensitives = {};
+        for (var k in parsed.report.food.nutrients) {
+          switch (k.name) {
+            case "Sodium, Na":
+              sensitives.sodium = k.value;
+              break;
+            case "Phosphorus, P":
+              sensitives.phosphorus = k.value;
+              break;
+            case "Potassium, K":
+              sensitives.potassium = k.value;
+              break;
+          }
+        }
+        context.advice = "Sodium: " + sensitives.sodium +
+          ", Phosphorous: " + sensitives.phosphorous +
+          ", Potassium: " + sensitives.potassium;
+        console.log("--------" + context.advice);
+        callback(context);
+        // callback({
+        //     sodium: sensitives.sodium,
+        //     phosphorous: sensitives.phosphorus,
+        //     potassium: sensitives.potassium,
+        // });
+      });
+  });
+});
+
+const getFoodId = ((context, callback) => {
+  console.log("food id started");
+    var food = callback.food;
+    http.get({
+        host: 'api.nal.usda.gov',
+        path: '/ndb/search/?format=json&q=' + food
+        + '&sort=n&max=25&offset=0&api_key=hhrhGfhytRdiE3nDCPKuKU1xx1t3u1eGGFcz2igy'
+    }, function(response) {
+      console.log("response");
+      // Continuously update stream with data
+      var body = '';
+      response.on('data', function(d) {
+        // console.log(d);
+        body += d;
+      });
+      response.on('end', function() {
+        // Data reception is done, do whatever with it!
+        var parsed = JSON.parse(body);
+        console.log(parsed.errors.error[0]);
+          // var sensitives = {};
+          // for (var k in parsed.nutrients) {
+          //   switch (k.name) {
+          //     case "Sodium, Na":
+          //       sensitives.sodium = k.value;
+          //       break;
+          //     case "Phosphorus, P":
+          //       sensitives.phosphorus = k.value;
+          //       break;
+          //     case "Potassium, K":
+          //       sensitives.potassium = k.value;
+          //   }
+          // }
+
+          // callback({
+          //     sodium: sensitives.sodium,
+          //     phosphorous: sensitives.phosphorus,
+          //     potassium: sensitives.potassium,
+          // });
+          const id = "1";
+          getNutrientFacts(id, context, callback);
+      });
+    });
+});
+
+
+
+
+
+
 
 const token = (() => {
   if (process.argv.length !== 3) {
@@ -38,7 +136,12 @@ const actions = {
       console.log(food);
       context.food = food;
     }
-    cb(context);
+    if (!context.food) {
+      context.food = "bananas";
+    }
+    console.log(context);
+    getFoodId(context, cb);
+    // cb(context);
   },
   error(sessionId, context, error) {
     console.log(error.message);
@@ -46,6 +149,8 @@ const actions = {
   ['suggest'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
+    getNutrientFacts(context, cb);
+
     context.advice = 'don\'t';
     cb(context);
   },
